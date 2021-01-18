c66601a62ec9f10f7287dd498a32086007a7cf5c
Yoimer Román <ydrf19@gmail.com>
2021-01-18 14:05:42 -0400

    added buzzer folder including buzzer.py, it works fine on Raspberry Pi

---
+++ buzzer/buzzer.py
@@ -0,0 +1,15 @@
+# wiring
+# https://projects.raspberrypi.org/en/projects/physical-computing/8
+
+from gpiozero import Buzzer
+from time import sleep
+
+buzzer = Buzzer(17)
+
+while True:
+    print("Buzzer is ON")
+    buzzer.on()
+    sleep(1)
+    buzzer.off()
+    print("Buzzer is OFF")
+    sleep(1)
