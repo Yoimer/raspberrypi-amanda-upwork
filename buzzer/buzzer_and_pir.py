bc606d785d9261f7729801dff73a1b96dd13e594
Yoimer Román <ydrf19@gmail.com>
2021-01-21 21:01:58 -0400

    updated buzzer_and_pir using GPIO.BCM

--- buzzer/buzzer_and_pir.py
+++ buzzer/buzzer_and_pir.py
@@ -5,40 +5,42 @@
 # pir black ----> 39 RPI (GND)
 
 # pins configuration (buzzer)
-# positive on buzzer     GPIO(27) 13 on pi
+# positive on buzzer     GPIO(27) 27 on pi
 # negative on buzzer     GPIO(39) on pi (GND)
 
 import RPi.GPIO as GPIO
 import time
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.IN)                 #Read output from PIR motion sensor
-GPIO.setup(3, GPIO.OUT)                 #LED output pin
-GPIO.setup(13, GPIO.OUT)                #Set buzzer - pin 27 (13) as output
+
+# for GPIO numbering, choose BCM  
+GPIO.setmode(GPIO.BCM)  
+GPIO.setup(17, GPIO.IN)                 #Read output from PIR motion sensor
+GPIO.setup(2, GPIO.OUT)                 #LED output pin
+GPIO.setup(27, GPIO.OUT)                #Set buzzer - pin 27 (27) as output
 
 def triggerbuffer():
     #beeping
-    GPIO.output(13, 1)
+    GPIO.output(27, GPIO.HIGH)
     print ("Beeping")
     time.sleep(0.5)
     #no beeping
-    GPIO.output(13, 0)
-    #print ("No Beep")
+    GPIO.output(27, GPIO.LOW)
+    print ("No Beep")
     time.sleep(0.5)
 
 
 while True:
-    i=GPIO.input(11)
+    i=GPIO.input(17)
     if i == 0:                             #When output from motion sensor is LOW
         print("No intruders",i)
-        GPIO.output(3, 0)                #Turn OFF LED
+        GPIO.output(2, GPIO.LOW)          #Turn OFF LED
         time.sleep(0.1)                      # Delay in seconds
         #no beeping
-        GPIO.output(13, 0)
-
+        #GPIO.output(27, 0)
+        GPIO.output(27, GPIO.LOW)
     elif i == 1:                           #When output from motion sensor is HIGH
-        while (GPIO.input(11) == 1):
+        while (GPIO.input(17) == 1):
             print("Intruder detected",i)
-            GPIO.output(3, 1)                #Turn ON LED
+            GPIO.output(2, GPIO.HIGH)      #Turn ON LED
             time.sleep(0.1)
             triggerbuffer();
