85e45ef3647ae5130d9eabdc76b57487dfe64a39
Yoimer Román <ydrf19@gmail.com>
2021-02-03 23:07:03 -0400

    added printing information on motor_hat.py

--- motor-hat/motor_hat.py
+++ motor-hat/motor_hat.py
@@ -22,37 +22,45 @@ kit.motor2.throttle = 1.0
 # motors are running. After 1.0 sec., the
 # lines after sleep will set throttle to zero,
 # effectively turning the motor off.
+print("Forward at full throttle")
 time.sleep(1.0)
 # Stop & sleep for 1 sec.
 kit.motor1.throttle = 0.0
 kit.motor2.throttle = 0.0
+print("Stop & sleep for 1 sec.")
 time.sleep(1.0)
 
 # Right at half speed
 kit.motor1.throttle = 0.5
 kit.motor2.throttle = -0.5
 # let motors run for 2.0 seconds.
+print("Right Motor at half speed")
 time.sleep(2.0)
 # Stop. Sleep for 3 sec.
 kit.motor1.throttle = 0.0
 kit.motor2.throttle = 0.0
+print("Stop. Sleep for 3 sec.")
 time.sleep(3.0)
 
 # Backward at 3/4 speed
 kit.motor1.throttle = -0.75
 kit.motor2.throttle = -0.75
 # let motors run for 2.5 seconds
+print("Backward at 3/4 speed")
 time.sleep(2.5)
 # Stop. Sleep for 1 sec.
 kit.motor1.throttle = 0.0
 kit.motor2.throttle = 0.0
+print("Stop. Sleep for 1 sec.")
 time.sleep(1.0)
 
 # Forward rt at full speed, backward left at 1/4 speed
 kit.motor1.throttle = 1.0
 kit.motor2.throttle = -0.25
 # let motors run for 3 seconds
+print("Forward rt at full speed, backward left at 1/4 speed")
 time.sleep(3.0)
 # Stop. No need for a sleep here because we're done.
 kit.motor1.throttle = 0.0
 kit.motor2.throttle = 0.0
+print("Stop. No need for a sleep here because we're done")
